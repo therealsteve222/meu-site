5b8330aac2682c1d225e492059b1227375a2580e
therealsteve222 <steverainzhn@gmail.com>
2024-08-24 04:11:42 -0300

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,6 @@
+<script>
+  function redirect() {
+    // Substitua 'https://www.exemplo.com' pela URL desejada
+    window.location.href = 'https://pay.therealbrainsyncproject.com/bsp-checkout';
+  }
+</script>

127d88289a90a3aedf9fdf8f236c5d283ac78c0b
therealsteve222 <steverainzhn@gmail.com>
2024-08-24 04:17:38 -0300

    Delete redirect_prckt.js

--- redirect_prckt.js
+++
@@ -1,6 +0,0 @@
-<script>
-  function redirect() {
-    // Substitua 'https://www.exemplo.com' pela URL desejada
-    window.location.href = 'https://pay.therealbrainsyncproject.com/bsp-checkout';
-  }
-</script>
